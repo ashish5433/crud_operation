61892efe899756415dc7400283d553abbb551b9e
Ashish <lit2021025@iiitl.ac.in>
2025-01-17 15:52:16 +0530

    minor bug fixed

--- routes/route.js
+++ routes/route.js
@@ -5,8 +5,8 @@ import { authenticate,authorize } from '../middlewares/middleware.js';
 const route = express.Router();
 
 route.post('/', createUser)
-route.get('/',authenticate,getUser)
-route.put('/:id',authenticate,authorize('admin'),updateUser)
-route.delete('/:id',authenticate,authorize('admin'),deleteUser)
+route.get('/getusers',authenticate,getUser)
+route.put('/updateuser/:id',authenticate,authorize('admin'),updateUser)
+route.delete('/deleteuser/:id',authenticate,authorize('admin'),deleteUser)
 
 export default route
